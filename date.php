52a6dffa00da3190ab36bd73673d99bd90dbdbc1
DENISE LEONG <Your EMAILhauyee213024@dasein.edu.my>
2023-03-02 20:45:50 +0800

    descending php created

---
+++ date.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+   <?php
+   $t = date("Y");
+
+   if ($t > 2020) {
+      echo "Have a great year ahead!";
+   } else {
+      echo "Happy new year";
+   }
+   ?>
+   <?php
+   $y = date("d");
+   ?>
+
+
+
+</body>
+
+</html>
---
+++ descending.php
@@ -0,0 +1,6 @@
+for ($i = 100; $i >= 1; $i--) {
+  if ($i % 2 == 0) {
+    continue; // skip even numbers
+  }
+  echo $i . "<br>"; // print odd numbers
+}
