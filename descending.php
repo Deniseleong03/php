f386e4f065781145fa58158fb07c8b0943b025b4
DENISE LEONG <Your EMAILhauyee213024@dasein.edu.my>
2023-03-04 20:56:20 +0800

    done

--- descending.php
+++ descending.php
@@ -1,6 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
 for ($i = 100; $i >= 1; $i--) {
-  if ($i % 2 == 0) {
-    continue; // skip even numbers
+    if ($i % 2 == 0) {
+      continue; // skip even numbers
+    }
+    echo $i . "<br>"; // print odd numbers
   }
-  echo $i . "<br>"; // print odd numbers
-}
+?>
+
+</body>
+</html>
+
